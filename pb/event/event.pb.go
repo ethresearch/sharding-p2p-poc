34cbd6f007db67d4c140abfb9d696a4a355cac59
mhchia <kevin.mh.chia@gmail.com>
2018-08-26 22:59:51 +0800

    Modify `event.proto`
    TODO
            - Add CallRPCSendCollation

--- pb/event/event.pb.go
+++ pb/event/event.pb.go
@@ -27,24 +27,24 @@ const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 type Response_Status int32
 
 const (
-	Response_VALID   Response_Status = 0
-	Response_INVALID Response_Status = 1
+	Response_SUCCESS Response_Status = 0
+	Response_FAILURE Response_Status = 1
 )
 
 var Response_Status_name = map[int32]string{
-	0: "VALID",
-	1: "INVALID",
+	0: "SUCCESS",
+	1: "FAILURE",
 }
 var Response_Status_value = map[string]int32{
-	"VALID":   0,
-	"INVALID": 1,
+	"SUCCESS": 0,
+	"FAILURE": 1,
 }
 
 func (x Response_Status) String() string {
 	return proto.EnumName(Response_Status_name, int32(x))
 }
 func (Response_Status) EnumDescriptor() ([]byte, []int) {
-	return fileDescriptor_event_ef0e8db394a9f730, []int{2, 0}
+	return fileDescriptor_event_790287848f21d87c, []int{3, 0}
 }
 
 type MetaMsg struct {
@@ -57,7 +57,7 @@ func (m *MetaMsg) Reset()         { *m = MetaMsg{} }
 func (m *MetaMsg) String() string { return proto.CompactTextString(m) }
 func (*MetaMsg) ProtoMessage()    {}
 func (*MetaMsg) Descriptor() ([]byte, []int) {
-	return fileDescriptor_event_ef0e8db394a9f730, []int{0}
+	return fileDescriptor_event_790287848f21d87c, []int{0}
 }
 func (m *MetaMsg) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_MetaMsg.Unmarshal(m, b)
@@ -77,7 +77,7 @@ func (m *MetaMsg) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_MetaMsg proto.InternalMessageInfo
 
-type CollationNotify struct {
+type NewCollationNotify struct {
 	MetaMsg              *MetaMsg           `protobuf:"bytes,1,opt,name=metaMsg,proto3" json:"metaMsg,omitempty"`
 	Collation            *message.Collation `protobuf:"bytes,2,opt,name=collation,proto3" json:"collation,omitempty"`
 	XXX_NoUnkeyedLiteral struct{}           `json:"-"`
@@ -85,42 +85,88 @@ type CollationNotify struct {
 	XXX_sizecache        int32              `json:"-"`
 }
 
-func (m *CollationNotify) Reset()         { *m = CollationNotify{} }
-func (m *CollationNotify) String() string { return proto.CompactTextString(m) }
-func (*CollationNotify) ProtoMessage()    {}
-func (*CollationNotify) Descriptor() ([]byte, []int) {
-	return fileDescriptor_event_ef0e8db394a9f730, []int{1}
+func (m *NewCollationNotify) Reset()         { *m = NewCollationNotify{} }
+func (m *NewCollationNotify) String() string { return proto.CompactTextString(m) }
+func (*NewCollationNotify) ProtoMessage()    {}
+func (*NewCollationNotify) Descriptor() ([]byte, []int) {
+	return fileDescriptor_event_790287848f21d87c, []int{1}
 }
-func (m *CollationNotify) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_CollationNotify.Unmarshal(m, b)
+func (m *NewCollationNotify) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_NewCollationNotify.Unmarshal(m, b)
 }
-func (m *CollationNotify) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_CollationNotify.Marshal(b, m, deterministic)
+func (m *NewCollationNotify) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_NewCollationNotify.Marshal(b, m, deterministic)
 }
-func (dst *CollationNotify) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_CollationNotify.Merge(dst, src)
+func (dst *NewCollationNotify) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_NewCollationNotify.Merge(dst, src)
 }
-func (m *CollationNotify) XXX_Size() int {
-	return xxx_messageInfo_CollationNotify.Size(m)
+func (m *NewCollationNotify) XXX_Size() int {
+	return xxx_messageInfo_NewCollationNotify.Size(m)
 }
-func (m *CollationNotify) XXX_DiscardUnknown() {
-	xxx_messageInfo_CollationNotify.DiscardUnknown(m)
+func (m *NewCollationNotify) XXX_DiscardUnknown() {
+	xxx_messageInfo_NewCollationNotify.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_CollationNotify proto.InternalMessageInfo
+var xxx_messageInfo_NewCollationNotify proto.InternalMessageInfo
 
-func (m *CollationNotify) GetMetaMsg() *MetaMsg {
+func (m *NewCollationNotify) GetMetaMsg() *MetaMsg {
 	if m != nil {
 		return m.MetaMsg
 	}
 	return nil
 }
 
-func (m *CollationNotify) GetCollation() *message.Collation {
+func (m *NewCollationNotify) GetCollation() *message.Collation {
 	if m != nil {
 		return m.Collation
 	}
 	return nil
+}
+
+type NewCollationResponse struct {
+	Response             *Response `protobuf:"bytes,1,opt,name=response,proto3" json:"response,omitempty"`
+	IsValid              bool      `protobuf:"varint,2,opt,name=isValid,proto3" json:"isValid,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}  `json:"-"`
+	XXX_unrecognized     []byte    `json:"-"`
+	XXX_sizecache        int32     `json:"-"`
+}
+
+func (m *NewCollationResponse) Reset()         { *m = NewCollationResponse{} }
+func (m *NewCollationResponse) String() string { return proto.CompactTextString(m) }
+func (*NewCollationResponse) ProtoMessage()    {}
+func (*NewCollationResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_event_790287848f21d87c, []int{2}
+}
+func (m *NewCollationResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_NewCollationResponse.Unmarshal(m, b)
+}
+func (m *NewCollationResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_NewCollationResponse.Marshal(b, m, deterministic)
+}
+func (dst *NewCollationResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_NewCollationResponse.Merge(dst, src)
+}
+func (m *NewCollationResponse) XXX_Size() int {
+	return xxx_messageInfo_NewCollationResponse.Size(m)
+}
+func (m *NewCollationResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_NewCollationResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_NewCollationResponse proto.InternalMessageInfo
+
+func (m *NewCollationResponse) GetResponse() *Response {
+	if m != nil {
+		return m.Response
+	}
+	return nil
+}
+
+func (m *NewCollationResponse) GetIsValid() bool {
+	if m != nil {
+		return m.IsValid
+	}
+	return false
 }
 
 type Response struct {
@@ -134,7 +180,7 @@ func (m *Response) Reset()         { *m = Response{} }
 func (m *Response) String() string { return proto.CompactTextString(m) }
 func (*Response) ProtoMessage()    {}
 func (*Response) Descriptor() ([]byte, []int) {
-	return fileDescriptor_event_ef0e8db394a9f730, []int{2}
+	return fileDescriptor_event_790287848f21d87c, []int{3}
 }
 func (m *Response) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Response.Unmarshal(m, b)
@@ -158,12 +204,13 @@ func (m *Response) GetStatus() Response_Status {
 	if m != nil {
 		return m.Status
 	}
-	return Response_VALID
+	return Response_SUCCESS
 }
 
 func init() {
 	proto.RegisterType((*MetaMsg)(nil), "proto.event.MetaMsg")
-	proto.RegisterType((*CollationNotify)(nil), "proto.event.CollationNotify")
+	proto.RegisterType((*NewCollationNotify)(nil), "proto.event.NewCollationNotify")
+	proto.RegisterType((*NewCollationResponse)(nil), "proto.event.NewCollationResponse")
 	proto.RegisterType((*Response)(nil), "proto.event.Response")
 	proto.RegisterEnum("proto.event.Response_Status", Response_Status_name, Response_Status_value)
 }
@@ -181,7 +228,7 @@ const _ = grpc.SupportPackageIsVersion4
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type EventClient interface {
 	// Sends a greeting
-	NewCollation(ctx context.Context, in *CollationNotify, opts ...grpc.CallOption) (*Response, error)
+	NewCollation(ctx context.Context, in *NewCollationNotify, opts ...grpc.CallOption) (*NewCollationResponse, error)
 }
 
 type eventClient struct {
@@ -192,8 +239,8 @@ func NewEventClient(cc *grpc.ClientConn) EventClient {
 	return &eventClient{cc}
 }
 
-func (c *eventClient) NewCollation(ctx context.Context, in *CollationNotify, opts ...grpc.CallOption) (*Response, error) {
-	out := new(Response)
+func (c *eventClient) NewCollation(ctx context.Context, in *NewCollationNotify, opts ...grpc.CallOption) (*NewCollationResponse, error) {
+	out := new(NewCollationResponse)
 	err := c.cc.Invoke(ctx, "/proto.event.Event/NewCollation", in, out, opts...)
 	if err != nil {
 		return nil, err
@@ -204,7 +251,7 @@ func (c *eventClient) NewCollation(ctx context.Context, in *CollationNotify, opt
 // EventServer is the server API for Event service.
 type EventServer interface {
 	// Sends a greeting
-	NewCollation(context.Context, *CollationNotify) (*Response, error)
+	NewCollation(context.Context, *NewCollationNotify) (*NewCollationResponse, error)
 }
 
 func RegisterEventServer(s *grpc.Server, srv EventServer) {
@@ -212,7 +259,7 @@ func RegisterEventServer(s *grpc.Server, srv EventServer) {
 }
 
 func _Event_NewCollation_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(CollationNotify)
+	in := new(NewCollationNotify)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -224,7 +271,7 @@ func _Event_NewCollation_Handler(srv interface{}, ctx context.Context, dec func(
 		FullMethod: "/proto.event.Event/NewCollation",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(EventServer).NewCollation(ctx, req.(*CollationNotify))
+		return srv.(EventServer).NewCollation(ctx, req.(*NewCollationNotify))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -242,25 +289,28 @@ var _Event_serviceDesc = grpc.ServiceDesc{
 	Metadata: "event.proto",
 }
 
-func init() { proto.RegisterFile("event.proto", fileDescriptor_event_ef0e8db394a9f730) }
+func init() { proto.RegisterFile("event.proto", fileDescriptor_event_790287848f21d87c) }
 
-var fileDescriptor_event_ef0e8db394a9f730 = []byte{
-	// 263 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x6c, 0x90, 0xc1, 0x4b, 0xc3, 0x30,
-	0x14, 0xc6, 0x57, 0x61, 0xad, 0x7d, 0x15, 0x95, 0xa0, 0x50, 0x86, 0x87, 0x91, 0x93, 0x97, 0xa5,
-	0x50, 0xc5, 0xb3, 0xa2, 0x3b, 0x0c, 0x5c, 0x0f, 0x15, 0xbc, 0xa7, 0xf5, 0xd9, 0x16, 0xd6, 0x26,
-	0x34, 0x6f, 0xca, 0xfe, 0x7b, 0x31, 0x6d, 0xa6, 0x13, 0x4f, 0x8f, 0xbc, 0xfc, 0xbe, 0xef, 0x17,
-	0x02, 0x11, 0x7e, 0x60, 0x47, 0x42, 0xf7, 0x8a, 0x14, 0x8b, 0xec, 0x10, 0x76, 0x35, 0xbb, 0xaf,
-	0x1a, 0xaa, 0xb7, 0x85, 0x28, 0x55, 0x9b, 0x20, 0xd5, 0x3d, 0x1a, 0x94, 0x7d, 0x59, 0x27, 0xa6,
-	0x96, 0xfd, 0x5b, 0xd3, 0x55, 0x0b, 0x9d, 0xea, 0x85, 0x56, 0x65, 0xa2, 0x8b, 0xa4, 0x45, 0x63,
-	0x64, 0x85, 0x6e, 0x0e, 0x75, 0x3c, 0x84, 0x60, 0x8d, 0x24, 0xd7, 0xa6, 0xe2, 0x3b, 0x38, 0x7b,
-	0x54, 0x9b, 0x8d, 0xa4, 0x46, 0x75, 0x99, 0xa2, 0xe6, 0x7d, 0xc7, 0x04, 0x04, 0xed, 0x70, 0x1b,
-	0x7b, 0x73, 0xef, 0x3a, 0x4a, 0x2f, 0xc4, 0x2f, 0xbd, 0x18, 0x93, 0xb9, 0x83, 0xd8, 0x1d, 0x84,
-	0xa5, 0xab, 0x88, 0x8f, 0x6c, 0x22, 0x1e, 0x13, 0x4e, 0xbb, 0x57, 0xe4, 0x3f, 0x28, 0x2f, 0xe0,
-	0x38, 0x47, 0xa3, 0x55, 0x67, 0x90, 0xdd, 0x82, 0x6f, 0x48, 0xd2, 0xd6, 0x58, 0xe5, 0x69, 0x7a,
-	0x75, 0xa0, 0x74, 0x98, 0x78, 0xb1, 0x4c, 0x3e, 0xb2, 0x7c, 0x0e, 0xfe, 0xb0, 0x61, 0x21, 0x4c,
-	0x5f, 0x1f, 0x9e, 0x57, 0x4f, 0xe7, 0x13, 0x16, 0x41, 0xb0, 0xca, 0x86, 0x83, 0x97, 0x66, 0x30,
-	0x5d, 0x7e, 0x57, 0xb0, 0x25, 0x9c, 0x64, 0xf8, 0xb9, 0x7f, 0x07, 0x3b, 0x14, 0xfc, 0xf9, 0x82,
-	0xd9, 0xe5, 0xbf, 0x7a, 0x3e, 0x29, 0x7c, 0xbb, 0xbf, 0xf9, 0x0a, 0x00, 0x00, 0xff, 0xff, 0x16,
-	0x39, 0x98, 0x61, 0x9e, 0x01, 0x00, 0x00,
+var fileDescriptor_event_790287848f21d87c = []byte{
+	// 305 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x90, 0xcf, 0x4b, 0xc3, 0x30,
+	0x14, 0xc7, 0x37, 0xc1, 0x75, 0x7b, 0x15, 0x19, 0x61, 0x42, 0x19, 0x82, 0x9a, 0x93, 0x97, 0xa5,
+	0x58, 0xc5, 0xb3, 0x32, 0x26, 0x08, 0x6e, 0x87, 0xd4, 0x79, 0xf3, 0x90, 0x75, 0xb1, 0x0d, 0xac,
+	0x4d, 0x68, 0x32, 0x45, 0xf0, 0x8f, 0x97, 0xa5, 0x4d, 0xed, 0x50, 0x4f, 0xaf, 0xef, 0xf1, 0xf9,
+	0xfe, 0x68, 0xc0, 0xe7, 0xef, 0xbc, 0x30, 0x44, 0x95, 0xd2, 0x48, 0xe4, 0xdb, 0x41, 0xec, 0x69,
+	0x7c, 0x97, 0x0a, 0x93, 0x6d, 0x57, 0x24, 0x91, 0x79, 0xc8, 0x4d, 0x56, 0x72, 0xcd, 0x59, 0x99,
+	0x64, 0xa1, 0xce, 0x58, 0xb9, 0x16, 0x45, 0x3a, 0x51, 0x91, 0x9a, 0x28, 0x99, 0x84, 0x6a, 0x15,
+	0xe6, 0x5c, 0x6b, 0x96, 0x72, 0x37, 0x2b, 0x3b, 0x3c, 0x00, 0x6f, 0xce, 0x0d, 0x9b, 0xeb, 0x14,
+	0x7f, 0x01, 0x5a, 0xf0, 0x8f, 0xa9, 0xdc, 0x6c, 0x98, 0x11, 0xb2, 0x58, 0x48, 0x23, 0xde, 0x3e,
+	0x11, 0x01, 0x2f, 0xaf, 0x80, 0xa0, 0x7b, 0xde, 0xbd, 0xf4, 0xa3, 0x11, 0x69, 0x35, 0x20, 0xb5,
+	0x98, 0x3a, 0x08, 0xdd, 0xc2, 0x20, 0x71, 0x16, 0xc1, 0x81, 0x55, 0x04, 0xb5, 0xc2, 0x25, 0x37,
+	0x11, 0xf4, 0x07, 0xc5, 0x09, 0x8c, 0xda, 0xe9, 0x94, 0x6b, 0x25, 0x0b, 0xcd, 0xd1, 0x15, 0xf4,
+	0xcb, 0xfa, 0xbb, 0x2e, 0x70, 0xb2, 0x57, 0xc0, 0x81, 0xb4, 0xc1, 0x50, 0x00, 0x9e, 0xd0, 0x2f,
+	0x6c, 0x23, 0xd6, 0xb6, 0x40, 0x9f, 0xba, 0x15, 0xaf, 0xa1, 0xdf, 0x18, 0xdf, 0x40, 0x4f, 0x1b,
+	0x66, 0xb6, 0xda, 0xda, 0x1e, 0x47, 0xa7, 0x7f, 0xda, 0x92, 0xd8, 0x32, 0xb4, 0x66, 0x31, 0x86,
+	0x5e, 0x75, 0x41, 0x3e, 0x78, 0xf1, 0x72, 0x3a, 0x9d, 0xc5, 0xf1, 0xb0, 0xb3, 0x5b, 0x1e, 0xee,
+	0x1f, 0x9f, 0x96, 0x74, 0x36, 0xec, 0x46, 0xaf, 0x70, 0x38, 0xdb, 0x99, 0xa0, 0x67, 0x38, 0x6a,
+	0xff, 0x13, 0x3a, 0xdb, 0x8b, 0xf8, 0xfd, 0xd8, 0xe3, 0x8b, 0x7f, 0x01, 0xd7, 0x07, 0x77, 0x56,
+	0x3d, 0xcb, 0x5c, 0x7f, 0x07, 0x00, 0x00, 0xff, 0xff, 0x91, 0xc9, 0x79, 0x0a, 0x17, 0x02, 0x00,
+	0x00,
 }
